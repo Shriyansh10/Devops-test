de1875ba97648fe5fe4a9ac5f865c34c739f617e
Shriyansh007 <agarwalshriyansh007@gmail.com>
2026-06-09 02:07:24 +0530

    Update Docker container port mapping and server version in deployment workflow

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.json({ message: "Server is running v4" });
+  res.json({ message: "Server is running v5", owner: "Shriyansh Agarwal"});
 });
 
 app.get("/health", (req, res) => {
